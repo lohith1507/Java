c13eb686a8c0321d45ad80d244c369bc55fe1fd4
lohith1507 <lohith15@gmail.com>
2023-12-22 09:14:05 +0530

    calculator code

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,50 @@
+import java.util.*;
 
 public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double res;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter First number : ");
+		double a = sc.nextDouble();
+		
+		System.out.println("Choose any of the operator (+, -, *, /, % )");
+		char op = sc.next().charAt(0); 
+		
+		System.out.println("Enter Second Number");
+		double b = sc.nextDouble();
+		
+		
+		switch(op) {
+		case '+' : 
+			res = a+b;
+			System.out.println("A + B = " + res);
+			break;
+			
+		case '-' : 
+			res = a-b;
+			System.out.println("A - B = " + res);
+			break;
+			
+		case '*' : 
+			res = a*b;
+			System.out.println("A * B = " + res);
+			break;
+			
+		case '/' : 
+			res = a/b;
+			System.out.println("A / B = " + res);
+			break;
+			
+		case '%' : 
+			res = a%b;
+			System.out.println("A % B = " + res);
+			break;	
+				
+		}
+		
 
 	}
 
